e94895cad5c8390f1753ed717951754fe6bf65ce
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-13 11:17:54 -0400

    Added credential helper to git yet again#3

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,11 +1,8 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     userName = "ArtemiiAkhunov";
     userEmail = "artemii13052003@gmail.com";
-    extraConfig = {
-      credential.helper = "oauth";
-    };
   };
   programs.git-credential-oauth = {
     enable = true;
