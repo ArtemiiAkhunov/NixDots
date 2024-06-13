f232b3b1259b5c0df4bca6c3fb49f75fc5b6200b
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-13 19:04:59 -0400

    Added lualine to vim

--- nixos/modules/nixvim/plugins/lualine.nix
+++ nixos/modules/nixvim/plugins/lualine.nix
@@ -19,7 +19,7 @@
         {
           name.__raw = ''
             function()
-              local msg ""
+              local msg = ""
               local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
                 local clients = vim.lsp.get_active_clients()
                 if next(clients) == nil then
