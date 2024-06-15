a970f7eb6caee45a775993acf0b1bb0118f43199
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-14 20:40:04 -0400

    Added nvidia stuff

---
+++ nixos/modules/gpu.nix
@@ -0,0 +1,27 @@
+{config, lib, pckgs, ...}: {
+  
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  hardware.nvidia = {
+    
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
