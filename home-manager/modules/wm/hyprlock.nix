bf60cb4920be3203497053b2f45bb9549a110c98
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-18 19:15:30 -0400

    Changed colors and made the thing work

--- home-manager/modules/wm/hyprlock.nix
+++ home-manager/modules/wm/hyprlock.nix
@@ -24,35 +24,36 @@
         dots_center = true;
         outer_color = "rgba(0, 0, 0, 0)";
         inner_color = "rgba(0, 0, 0, 0.5)";
-        font_color = "rgb(245, 224, 220)";
+        font_color = "rgb(205, 214, 244)";
         fade_on_empty = false;
         font_family = "Inconsolata";
-        placeholder_text = ''<i><span foreground="##cba6f7">Input Password...</span></i>'';
+        placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
         hide_input = false;
         position = "0, -120";
         halign = "center";
         valign = "center";
       };
 
-      label = {
-        text = ''cmd[update:1000] echo "$(date +"%-I:%M%p")"'';
-        color = "rgb(245, 194, 231)";
-        font_size = 120;
-        font_family = "Inconsolata";
-        position = "0, -300";
-        halign = "center";
-        valign = "top";
-      };
-
-      label = {
-        text = "Hi there, $USER";
-        color = "rgb(245, 194, 231)";
-        font_size = 25;
-        font_family = "Inconsolata";
-        position = "0, -40";
-        halign = "center";
-        valign = "center";
-      };
+      label = [
+        {
+          text = ''cmd[update:1000] echo "$(date +"%-I:%M%p")"'';
+          color = "rgb(243, 139, 168)";
+          font_size = 120;
+          font_family = "Inconsolata";
+          position = "0, -300";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = "Hi there, $USER";
+          color = "rgb(243, 139, 168)";
+          font_size = 25;
+          font_family = "Inconsolata";
+          position = "0, -40";
+          halign = "center";
+          valign = "center";
+        }
+      ];
 
 
     };
