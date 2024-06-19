76e75c187bed96fa61602bdbd613f07c787e3be5
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-18 20:19:38 -0400

    Added a hyprlock on startup

--- home-manager/modules/wm/hyprland.nix
+++ home-manager/modules/wm/hyprland.nix
@@ -112,82 +112,83 @@
         "noanim,class:^(wofi)$" 
       ];
 
-     bind = [
-       # Basic Stuff
-       "$mainMod, Return, exec, $terminal"
-       "$mainMod SHIFT, Q, killactive,"
-       "$mainMod Control, Q, exit, "
-       "$mainMod SHIFT, F, togglefloating,"
-       "$mainMod, F, fullscreen, 0"
-       "$mainMod, D, exec, $menu"
-       "$mainMod, B, exec, $browser"
-       "$mainMod SHIFT, B, exec, $privateBrowser"
-       # Utilities
-       ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-       ''$mainMod SHIFT, Print, exec, grim -g "$(slurp)" - | swappy -f - ''
-       ''$mainMod, Print, exec, grim - | swappy -f -''
-       "$mainMod SHIFT, L, exec, hyprlock"
-       "$mainMod SHIFT, M, exec, dunstctl set-paused toggle"
-       # Moving Around
-       "$mainMod, left, movefocus, l"
-       "$mainMod, right, movefocus, r"
-       "$mainMod, up, movefocus, u"
-       "$mainMod, down, movefocus, d"
-       "$mainMod, 1, workspace, 1"
-       "$mainMod, 2, workspace, 2"
-       "$mainMod, 3, workspace, 3"
-       "$mainMod, 4, workspace, 4"
-       "$mainMod, 5, workspace, 5"
-       "$mainMod, 6, workspace, 6"
-       "$mainMod, 7, workspace, 7"
-       "$mainMod, 8, workspace, 8"
-       "$mainMod, 9, workspace, 9"
-       "$mainMod, 0, workspace, 10"
-       "$mainMod SHIFT, 1, movetoworkspace, 1"
-       "$mainMod SHIFT, 2, movetoworkspace, 2"
-       "$mainMod SHIFT, 3, movetoworkspace, 3"
-       "$mainMod SHIFT, 4, movetoworkspace, 4"
-       "$mainMod SHIFT, 5, movetoworkspace, 5"
-       "$mainMod SHIFT, 6, movetoworkspace, 6"
-       "$mainMod SHIFT, 7, movetoworkspace, 7"
-       "$mainMod SHIFT, 8, movetoworkspace, 8"
-       "$mainMod SHIFT, 9, movetoworkspace, 9"
-       "$mainMod SHIFT, 0, movetoworkspace, 10"
-       # Special Workspaces
-       "$mainMod, M, togglespecialworkspace, magic"
-       "$mainMod SHIFT, S, togglespecialworkspace, schedule"
-       # Mouse Moving
-       "$mainMod, mouse_down, workspace, e+1"
-       "$mainMod, mouse_up, workspace, e-1"
-     ];
+      bind = [
+        # Basic Stuff
+        "$mainMod, Return, exec, $terminal"
+        "$mainMod SHIFT, Q, killactive,"
+        "$mainMod Control, Q, exit, "
+        "$mainMod SHIFT, F, togglefloating,"
+        "$mainMod, F, fullscreen, 0"
+        "$mainMod, D, exec, $menu"
+        "$mainMod, B, exec, $browser"
+        "$mainMod SHIFT, B, exec, $privateBrowser"
+        # Utilities
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ''$mainMod SHIFT, Print, exec, grim -g "$(slurp)" - | swappy -f - ''
+        ''$mainMod, Print, exec, grim - | swappy -f -''
+        "$mainMod SHIFT, L, exec, hyprlock"
+        "$mainMod SHIFT, M, exec, dunstctl set-paused toggle"
+        # Moving Around
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+        # Special Workspaces
+        "$mainMod, M, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, togglespecialworkspace, schedule"
+        # Mouse Moving
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+      ];
 
-     binde = [
-       ",XF86MonBrightnessUp, exec, brightnessctl set 5%+"
-       ",XF86MonBrightnessDown, exec, brightnessctl set 5%-"
-       ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
-       ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
-       "$mainMod SHIFT, H, exec, dunstctl history-pop"
-     ];
+      binde = [
+        ",XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-"
+        "$mainMod SHIFT, H, exec, dunstctl history-pop"
+      ];
     
-     bindm = [
-       "$mainMod, mouse:272, movewindow"
-       "$mainMod, mouse:273, resizewindow"
-     ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
 
-     exec-once = [
-       "swww-daemon"
-       "swww img ~/.wallpaper/ryo2.jpg --transition-type none"
-       "waybar"
-       "xwaylandwideobringe"
-       "dunst"
-       "lxpolkit"
-       "copyq --start-server"
-     ];
+      exec-once = [
+        "swww-daemon"
+        "swww img ~/.wallpaper/ryo2.jpg --transition-type none"
+        "waybar"
+        "xwaylandwideobringe"
+        "dunst"
+        "lxpolkit"
+        "copyq --start-server"
+        "hyprlock"
+      ];
 
-     workspace = [
-       "special:magic, gapsin:40, gapsout:60"
-       "special:schedule, on-created-empty:$openSchedule"
-     ];
+      workspace = [
+        "special:magic, gapsin:40, gapsout:60"
+        "special:schedule, on-created-empty:$openSchedule"
+      ];
     };
   };
 }
