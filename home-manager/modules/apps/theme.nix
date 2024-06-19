b91e4247f82b974c59d49f3fc98ee8944d68058b
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-19 19:57:47 -0400

    Changed the theme to mocha

--- home-manager/modules/apps/theme.nix
+++ home-manager/modules/apps/theme.nix
@@ -1,7 +1,21 @@
-{
+{pkgs, ...}: {
   catppuccin = {
     flavor = "mocha";
     enable = true;
     accent="lavender";
+  };
+  
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "palenight";
+      package = pkgs.palenight-theme;
+    };
   };
 }
