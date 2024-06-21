2839ade271020401716fa33735b14d803d88bfba
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-21 16:59:50 -0400

    Added hosts

--- nixos/modules/hosts.nix
+++ nixos/modules/hosts.nix
@@ -1,8 +1,9 @@
 {
-  networking.extraHosts = [
-    "http://192.168.5.58 pi-hole"
-    "https://192.168.5.58:9443 portainer"
-    "http://192.168.5.58:3000 chatgpt"
-    "http://192.168.5.58:8096 jellyfin"
-  ];
+  networking.extraHosts = 
+    ''
+      http://192.168.5.58 pi-hole
+      https://192.168.5.58:9443 portainer
+      http://192.168.5.58:3000 chatgpt
+      http://192.168.5.58:8096 jellyfin
+    '';
 }
