3d0788a20c4e94eeccbcd73b02ed09fa893a1bb9
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-26 07:44:14 -0400

    Fixed the issue with davinci?

--- home-manager/modules/apps/desktop-entries.nix
+++ home-manager/modules/apps/desktop-entries.nix
@@ -3,7 +3,7 @@
     davinci-resolve = {
       name = "Davinci Resolve";
       genericName = "Video Editor";
-      exec = "${pkgs.davinci-resolve}/bin/davinci-resolve";
+      exec = "nvidia-offload ${pkgs.davinci-resolve}/bin/davinci-resolve";
       terminal = false;
       categories = [ "Application" "AudioVideo" "Video" "Utility" ];
     };
