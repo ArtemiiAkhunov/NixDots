271464b885bcd863353556117ed3ac0969496feb
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-28 12:31:22 -0400

    Tweaking with nvim

--- nixos/modules/nixvim/plugins/dap.nix
+++ nixos/modules/nixvim/plugins/dap.nix
@@ -1,29 +1,86 @@
-{
-  programs.nixvim.plugins.dap = {
-    enable = true;
-    signs = {
-      dapBreakpoint = {
-        text = "●";
-        texthl = "DapBreakpoint";
-      };
-      dapBreakpointCondition = {
-        text = "●";
-        texthl = "DapBreakpointCondition";
-      };
-      dapLogPoint = {
-        text = "◆";
-        texthl = "DapLogPoint";
-      };
-    };
-    extensions = {
-      dap-ui = {
-        enable = true;
-        floating.mappings = {
-          close = ["<ESC>" "q"];
+{pkgs, ...}: {
+  programs.nixvim = {
+    plugins.dap = {
+      enable = true;
+    
+      signs = {
+        dapBreakpoint = {
+          text = "●";
+          texthl = "DapBreakpoint";
+        };
+        dapBreakpointCondition = {
+          text = "●";
+          texthl = "DapBreakpointCondition";
+        };
+        dapLogPoint = {
+          text = "◆";
+          texthl = "DapLogPoint";
         };
       };
-      dap-python.enable = true;
-      dap-virtual-text.enable = true;
+
+      extensions = {
+        dap-ui = {
+          enable = true;
+          floating.mappings = {
+            close = ["<ESC>" "q"];
+          };
+        };
+        dap-python.enable = true;
+        dap-virtual-text.enable = true;
+      };
     };
+    extraConfigLua = '' 
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+      
+      dapui.setup()
+
+      dap.adapters.lldb = {
+        type = 'executable',
+        command = '${pkgs.lldb_18}/bin/lldb-vscode', -- adjust as needed, must be absolute path
+        name = 'lldb'
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = 'Launch',
+          type = 'lldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '$${workspaceFolder}',
+          stopOnEntry = false,
+          args = {},
+
+          -- 💀
+          -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+          --
+          --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+          --
+          -- Otherwise you might get the following error:
+          --
+          --    Error on launch: Failed to attach to the target process
+          --
+          -- But you should be aware of the implications:
+          -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+          -- runInTerminal = false,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+    '';
   };
 }
