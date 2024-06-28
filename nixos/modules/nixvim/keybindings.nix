b78da9d67cb4749dbfc2d863e5c74372ecdabaa3
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-28 12:38:55 -0400

    Tweaking with nvim

--- nixos/modules/nixvim/keybindings.nix
+++ nixos/modules/nixvim/keybindings.nix
@@ -22,12 +22,15 @@
           # Save by ctrl + s
           "<C-s>" = ":w<CR>";
 
-          # Debugger, maybe working?
+          # Debugger
 
           "<leader>dt" = ":lua require('dap').toggle_breakpoint()<CR>";
 
           "<leader>dc" = ":lua require('dap').continue()<CR>";
 
+          # Formatting
+
+          "<leader>fo" = ":lua vim.lsp.buf.format()<CR>";
         };
       visual = 
         lib.mapAttrsToList
