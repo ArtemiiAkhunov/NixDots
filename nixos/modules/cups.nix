ad1791554b502e18264293eca74cd06aab346151
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-06-30 16:06:00 -0400

    Apparently works-for-me is a crappy excuse.

--- nixos/modules/cups.nix
+++ nixos/modules/cups.nix
@@ -1,11 +1,41 @@
 { pkgs, ... }: {
   services.printing = {
     enable = true;
-    drivers = [ pkgs.gutenprint pkgs.gutenprintBin pkgs.canon-cups-ufr2 ];
+    drivers = with pkgs; [
+      gutenprint
+      gutenprintBin
+      hplip
+    ];
+  };
+
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "HP_WiFi";
+        location = "Home";
+        deviceUri = "http://192.168.178.2:631/printers/";
+        model = "HP/hp-laserjet_tank_mfp_260x.ppd.gz";
+        ppdOptions = {
+          PageSize = "Letter";
+        };
+      }
+      {
+         name = "HP_USB";
+        location = "Home";
+        deviceUri = "http://192.168.178.2:631/printers/";
+        model = "HP/hp-laserjet_tank_mfp_260x.ppd.gz";
+        ppdOptions = {
+          PageSize = "Letter";
+        };
+
+      }
+    ];
+    ensureDefaultPrinter = "HP_WiFi";
   };
 
   services.avahi = {
     enable = true;
-    nssmdns4 = true;
+    nssmdns = true;
+    openFirewall = true;
   };
 }
