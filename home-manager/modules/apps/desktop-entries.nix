946d5ee4c870c02f1fe74376da8013df7ac4ab8a
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-07-03 16:00:07 -0400

    Changed the davinci desktop file

--- home-manager/modules/apps/desktop-entries.nix
+++ home-manager/modules/apps/desktop-entries.nix
@@ -5,7 +5,8 @@
       genericName = "Video Editor";
       exec = "nvidia-offload ${pkgs.davinci-resolve}/bin/davinci-resolve";
       terminal = false;
-      categories = [ "Application" "AudioVideo" "Video" "Utility" ];
+      icon = "${pkgs.papirus-nord}/share/icons/Papirus/16x16/apps/resolve.svg";
+      categories = [ "AudioVideo" "Video" "Utility" ];
     };
   };
 }
