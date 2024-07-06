fa782b79542e9583be4b28adcdd7c24cf13e3ff0
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-07-06 17:32:56 -0400

    Adding a file manager

---
+++ nixos/modules/filemanager.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  programs = {
+    xfconf.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-volman
+        thunar-archive-plugin
+      ];
+    };
+  };
+}
