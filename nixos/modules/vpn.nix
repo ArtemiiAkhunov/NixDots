eea2b71aa6a6f3fc456e17b634001d4d7efa163e
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-07-08 14:04:32 -0400

    Adding a vpn

--- nixos/modules/vpn.nix
+++ nixos/modules/vpn.nix
@@ -1,6 +1,8 @@
 {
   services.openvpn.servers = {
-    config = '' config /home/voidwalker/.vpn/openvpn/buffalo.conf '';
+    buffaloVPN = {
+      config = '' config /home/voidwalker/.vpn/openvpn/buffalo.conf '';
       updateResolvConf = true;
+    };
   };
 }
