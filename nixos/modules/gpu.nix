cdb39b3501d1d19d1d554fdef5f06e5d74c0fd09
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-16 12:02:29 -0400

    Added hardware encoding

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -1,11 +1,22 @@
-{config, ...}: {
+{config, pkgs, ...}: {
 
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      intel-vaapi-driver
+    ];
   };
+
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
 
   services.xserver.videoDrivers = [ "nvidia" ];
   
