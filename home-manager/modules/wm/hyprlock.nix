1065f54219ac14b157c886a86c3c7a8476f28271
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-19 10:17:05 -0400

    Updated the wallpaper

--- home-manager/modules/wm/hyprlock.nix
+++ home-manager/modules/wm/hyprlock.nix
@@ -11,7 +11,7 @@
         ignore_empty_input = true;
       };
       background = {
-        path = "$HOME/.wallpaper/nixchan.png";
+        path = "$HOME/.wallpaper/cat.png";
         blur_passes = 3;
         blur_size = 8;
       };
