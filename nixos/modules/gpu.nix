b5fc672e64baf97e6ee3103180415b8e27d712be
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-20 11:35:17 -0400

    Changed user space drivers to latest

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -32,7 +32,7 @@
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
 
     prime = {
       offload = {
