43de2fcf2710cb915f6de16e080888c8aaa20c5e
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-21 15:47:02 -0400

    Trying to apply patches yet again

--- nixos/modules/fprintd.nix
+++ nixos/modules/fprintd.nix
@@ -3,7 +3,7 @@
     enable = true;
     tod = {
       enable = true;
-      driver = pkgs.libfprint-tod.overrideAttrs (previousAttrs: {
+      driver = pkgs.libfprint-tod.overrideAttrs (finalAttrs: previousAttrs: {
         patches = (previousAttrs.patches or []) ++ [
           (pkgs.fetchpatch {
             url = "https://gitlab.freedesktop.org/libfprint/libfprint/-/merge_requests/469.patch";
