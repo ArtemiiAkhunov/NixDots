c6528b78169780e03ee24936b6ad7fab58156409
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-21 13:17:07 -0400

    Changed to using just the Iris graphics

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -11,6 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelPackages = pkgs.linuxPackages_latest; # Linux Kernel Version
+  boot.kernelParams = [ "i915.force_probe=a7a0" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
