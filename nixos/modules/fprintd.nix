b58fda5fe3b12a6a552affd5135da747c2cedadf
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-21 15:44:44 -0400

    Fixed the fetchpatch?

--- nixos/modules/fprintd.nix
+++ nixos/modules/fprintd.nix
@@ -7,7 +7,7 @@
         patches = (previousAttrs.patches or []) ++ [
           (pkgs.fetchpatch {
             url = "https://gitlab.freedesktop.org/libfprint/libfprint/-/merge_requests/469.patch";
-            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+            sha256 = "sha256-tZ+JPGmX4KiNfxiQu4fj+ve3T3PbIOXZrBqkAEBLfC4";
           })
         ];
       });
