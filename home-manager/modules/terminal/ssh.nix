443e3b7ea798754bece569dd86b48011f3c9c43e
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-23 15:55:53 -0400

    updated ssh config

--- home-manager/modules/terminal/ssh.nix
+++ home-manager/modules/terminal/ssh.nix
@@ -1,22 +1,24 @@
 {
-  programs.ssh = {
+  programs.ssh = let 
+    rsa_key_path = "/home/voidwalker/.ssh/id_ed25519";
+    in {
     enable = true;
     matchBlocks = {
       theros = {
         hostname = "192.168.5.58";
         user = "voidwalker";
-        identityFile = "/home/voidwalker/.ssh/id_rsa";
+        identityFile = rsa_key_path;
         port = 2222;
       };
       data = {
         hostname = "data.cs.purdue.edu";
         user = "aakhunov";
-        identityFile = "/home/voidwalker/.ssh/id_ed25519";
+        identityFile = rsa_key_path;
       };
       xinu = {
         hostname = "xinu13.cs.purdue.edu";
         user = "aakhunov";
-        identityFile = "/home/voidwalker/.ssh/id_ed25519";
+        identityFile = rsa_key_path;
       };
     };
   };
