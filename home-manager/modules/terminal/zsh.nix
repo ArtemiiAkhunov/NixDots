dac81aabebbbd85448fb5b341c6755e153dc4ebf
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-08-27 19:36:41 -0400

    Removed conda

--- home-manager/modules/terminal/zsh.nix
+++ home-manager/modules/terminal/zsh.nix
@@ -29,20 +29,6 @@
 
     initExtra = ''
       fastfetch
-      # >>> conda initialize >>>
-      # !! Contents within this block are managed by 'conda init' !!
-      __conda_setup="$('/home/voidwalker/.conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-          eval "$__conda_setup"
-      else
-          if [ -f "/home/voidwalker/.conda/etc/profile.d/conda.sh" ]; then
-              . "/home/voidwalker/.conda/etc/profile.d/conda.sh"
-          else
-              export PATH="/home/voidwalker/.conda/bin:$PATH"
-          fi
-      fi
-      unset __conda_setup
-      # <<< conda initialize <<<
     '';
 
     oh-my-zsh = {
