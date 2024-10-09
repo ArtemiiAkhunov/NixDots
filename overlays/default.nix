688863b5ec17f64defd333c712fd0e1524ff4520
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-09 18:55:03 -0400

    Not compiling and still suffering with overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,16 @@
 {outputs, inputs}: {
+
+  flake-inputs = final: _: {
+    inputs = builtins.mapAttrs (
+      _: flake:
+      let
+        legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
+        packages = ((flake.packages or { }).${final.system} or { });
+      in
+      if legacyPackages != { } then legacyPackages else packages
+    ) inputs;
+  };
+
   modifications = final: prev: {
     libfprint= prev.libfprint.overrideAttrs (old: {
       src = builtins.fetchGit {
