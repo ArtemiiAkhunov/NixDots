f878dc0259a8352164e47543e7af30b267e6d236
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-09 19:04:18 -0400

    Prettifying everything

---
+++ nixos/machines/theros/configuration.nix
@@ -0,0 +1,11 @@
+{
+  time.timeZone = "America/Indianapolis"; #timezone
+
+  i18n.defaultLocale = "en_US.UTF-8"; #locale
+
+  networking.hostName = "theros";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Flakes
+
+  system.stateVersion = "24.05"; # DO NOT TOUCH
+}
