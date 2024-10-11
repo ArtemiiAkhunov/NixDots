e3e92c005e6e4c39e434b2f5467be264e392bce4
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-11 12:49:19 -0400

    Added gpg key and changed credentials?

--- home-manager/common/shell/git.nix
+++ home-manager/common/shell/git.nix
@@ -1,10 +1,18 @@
+{ pkgs, lib, ... }:
 {
   programs.git = {
     enable = true;
     userName = "ArtemiiAkhunov";
     userEmail = "artemii13052003@gmail.com";
-  };
-  programs.git-credential-oauth = {
-    enable = true;
+    signing = {
+      key = "A59548A0FB3DEC67B157E7471C21E057641FA4AC";
+      signByDefault = lib.mkDefault true;
+    };
+    extraConfig = {
+      push.autoSetupRemote = "true";
+      init.defaultBranch = "main";
+      pull.rebase = "true";
+      credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
+    };
   };
 }
