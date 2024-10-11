fa5bd5a262db632254c7a5cd2f10155b8a20e5bf
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-11 16:30:14 -0400

    Changed kernel version on the server

--- nixos/machines/theros/hardware-configuration.nix
+++ nixos/machines/theros/hardware-configuration.nix
@@ -21,7 +21,7 @@
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelPackages = pkgs.linuxPackages_hardened;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
