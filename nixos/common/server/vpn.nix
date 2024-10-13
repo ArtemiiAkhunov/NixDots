7b264c093d2f1ae4d8c85fb0c9c93c78c9ba3668
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-13 19:12:05 -0400

    TODO: fix vpn later

--- nixos/common/server/vpn.nix
+++ nixos/common/server/vpn.nix
@@ -6,31 +6,32 @@
 
   networking.wireguard.interfaces = {
     wg0 =
-    let
-      ip = "192.168.5.201";
-      subnet = "22";
-    in
-    {
-      ips = [ "${ip}/${subnet}" ];
+      let
+        ip = "192.168.5.201";
+        subnet = "22";
+      in
+      {
+        ips = [ "${ip}/${subnet}" ];
 
-      listenPort = 51820;
+        listenPort = 51820;
 
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${ip}/${subnet} -o eth0 -j MASQUERADE
-      '';
+        postSetup = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${ip}/${subnet} -o eth0 -j MASQUERADE
+        '';
 
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${ip}/${subnet} -o eth0 -j MASQUERADE
-      '';
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${ip}/${subnet} -o eth0 -j MASQUERADE
+        '';
 
-      privateKeyFile = config.age.secrets.wireguard-server.path;
+        privateKeyFile = config.age.secrets.wireguard-server.path;
 
-      peers = [
-        { # Laptop
-          publicKey = "H71T90I6YQdQH7Z3yFFztn9os3md9WfcGz7YTceQ1nI=";
-          allowedIPs = [ "${ip}/32" ]; 
-        }
-      ];
-    };
+        peers = [
+          {
+            # Laptop
+            publicKey = "H71T90I6YQdQH7Z3yFFztn9os3md9WfcGz7YTceQ1nI=";
+            allowedIPs = [ "${ip}/32" ];
+          }
+        ];
+      };
   };
 }
