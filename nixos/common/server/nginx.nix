ac9a3d158da3fbf092cae4bcc3907c4ebbef4ec6
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-15 13:01:04 -0400

    Attempt at starting an nginx proxy

--- nixos/common/server/nginx.nix
+++ nixos/common/server/nginx.nix
@@ -1,4 +1,17 @@
 { ... }:
 {
-  # TODO: nginx reverse proxy
+  /*
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      "matrix.lordofthelags.net" = {
+        forceSSL = true;
+        enableACME = true;
+        proxyPass = "http://localhost:8448";
+      };
+    };
+  };
+  */
 }
