5ad366778f870c1603f5954c344c3aeb2538b77a
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-14 20:55:24 -0400

    Changed firewall rules

--- nixos/common/server/firewall.nix
+++ nixos/common/server/firewall.nix
@@ -1,14 +1,9 @@
 {
   networking.firewall = {
     allowedTCPPorts = [
-      22
-      53
       80
       443
     ];
-    allowedUDPPorts = [
-      53
-      51820
-    ];
+    trustedInterfaces = [ "zteb4fawlt" ];
   };
 }
