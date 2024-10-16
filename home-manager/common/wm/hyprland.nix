f5bff9251eac0f263ff02e4c0f4c50dda8475dea
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-16 17:53:13 -0400

    Updated the adress to point to my custom domain

--- home-manager/common/wm/hyprland.nix
+++ home-manager/common/wm/hyprland.nix
@@ -8,7 +8,7 @@
       "$mainMod" = "$SUPER";
       "$terminal" = "${pkgs.kitty}/bin/kitty";
       "$browser" = "${pkgs.firefox}/bin/firefox";
-      "$privateBrowser" = "${pkgs.firefox}/bin/firefox --private-window lordofthelags.github.io/startpage/";
+      "$privateBrowser" = "${pkgs.firefox}/bin/firefox --private-window startpage.lordofthelags.net";
       "$openSchedule" = "${pkgs.imv}/bin/imv ~/Pictures/Schedule.png";
       "$menu" = "${pkgs.wofi}/bin/wofi --show drun";
 
