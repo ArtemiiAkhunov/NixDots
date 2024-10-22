b81ded00ddfb5d665fe86601f4621c237b5fabdd
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-10-22 09:05:35 -0400

    Updated the system

--- nixos/common/workstation/hyprland.nix
+++ nixos/common/workstation/hyprland.nix
@@ -15,8 +15,10 @@
     };
     pam.services.hyprlock.text = ''
       # PAM configuration file for the swaylock screen locker. By default, it includes
-      # the 'login' configuration file (see /etc/pam.d/login)
-      auth include login
+      # the 'login' configuration file (see /etc/pam.d/login) 
+      auth sufficient pam_unix.so likeauth try_first_pass # password
+      auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so max-tries=2 # frpint
+      auth required pam_deny.so # deny
     '';
   };
 }
