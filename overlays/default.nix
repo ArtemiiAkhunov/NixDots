190546676674122aec66676f3d1553e15a6d88ff
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-11-19 11:03:40 -0500

    Removed libfprintd overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,26 +13,8 @@
   };
 
   modifications = final: prev: {
-    libfprint = prev.libfprint.overrideAttrs (old: {
-      version = "1.94.8";
-      src = builtins.fetchGit {
-        url = "https://gitlab.freedesktop.org/libfprint/libfprint";
-        ref = "refs/heads/master";
-        rev = "e57bab2f1ef3583158d2e6e2b5bb4182629ebe35";
-      };
-    });
 
     nginxStable = prev.nginxStable.override { openssl = prev.pkgs.libressl; };
 
-    fprintd = prev.fprintd.overrideAttrs (old: {
-      mesonCheckFlags = [
-        # PAM related checks are timing out
-        "--no-suite"
-        "fprintd:TestPamFprintd"
-        # Tests FPrintdManagerPreStartTests.test_manager_get_no_default_device & FPrintdManagerPreStartTests.test_manager_get_no_devices are failing
-        "--no-suite"
-        "fprintd:FPrintdManagerPreStartTests"
-      ];
-    });
   };
 }
