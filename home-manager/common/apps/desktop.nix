64bde6b50394ec98e04fa456a0361ac6702c0551
ArtemiiAkhunov <artemii13052003@gmail.com>
2024-12-04 12:17:02 -0500

    Fixed the icon

--- home-manager/common/apps/desktop.nix
+++ home-manager/common/apps/desktop.nix
@@ -1,9 +1,17 @@
+{ pkgs, ... }:
+let
+  cura-icon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/Ultimaker/Cura/main/packaging/icons/cura-icon.svg";
+    sha256 = "sha256-ypqrZ/Wv/a+oyYHYtP8Aa/BeOBelTBoLcDEauiViJ7I=";
+  };
+in 
 {
   xdg.desktopEntries = {
     cura = {
       name = "Cura";
       genericName = "3D Printing Software";
       exec = "cura-appimage %U";
+      icon = cura-icon;
       terminal = false;
       categories = [ "Utility" ];
     };
