0a5857f68d5983ca61fa898ea3b58b660d79d0d6
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-01-19 17:12:12 -0500

    Added creality print?

--- packages/creality-print/default.nix
+++ packages/creality-print/default.nix
@@ -8,7 +8,7 @@ let
   version = "5.9.0";
   src = fetchurl {
     url = "https://github.com/CrealityOfficial/CrealityPrint/releases/download/v5.1.6/Creality_Print-v5.1.6.10470-x86_64-Release.AppImage";
-    hash = "";
+    hash = "sha256-gS7b41UEvjwUWjCAWjJbD5Xz8QblZkT6tv5ceRhjYfI=";
   };
   outputDerivation = appimageTools.wrapType2 {
     inherit pname version src;
