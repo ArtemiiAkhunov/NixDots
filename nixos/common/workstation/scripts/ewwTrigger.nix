263798b91a082a4b70c02d3fb4fda09b50373e3c
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-02-17 15:54:55 -0500

    Shell scripts are hard

--- nixos/common/workstation/scripts/ewwTrigger.nix
+++ nixos/common/workstation/scripts/ewwTrigger.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "ewwTrigger" ''
-  open=${pkgs.eww}/bin/eww active-windows | wc -w
-  if[ ((open == 0)) ]; then
+  open="$(${pkgs.eww}/bin/eww active-windows | wc -w)"
+  if ((open == 0)); then
     ${pkgs.eww}/bin/eww open activate-linux
   else
     ${pkgs.eww}/bin/eww close activate-linux
