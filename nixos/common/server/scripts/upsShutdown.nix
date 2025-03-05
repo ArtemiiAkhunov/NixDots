30665b8f9b8ce24fc9e6e87c3b50c17aeea26a99
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-03-05 08:42:21 -0500

    Changed the poweroff script for network ups tools

--- nixos/common/server/scripts/upsShutdown.nix
+++ nixos/common/server/scripts/upsShutdown.nix
@@ -3,7 +3,7 @@ let
   passwordFile = config.age.secrets.nut_admin_password.path;
 in
 pkgs.writeShellScriptBin "upsShutdown" ''
-  password="$(cat ${passwordFile})"
-  ${pkgs.nut}/bin/upscmd -u admin -p $password spark@localhost:3494 shutdown.return
-  ${pkgs.systemd}/bin/shutdown now
+  #password="$(cat ${passwordFile})"
+  #${pkgs.nut}/bin/upscmd -u admin -p $password spark@localhost:3494 shutdown.return
+  ${pkgs.systemd}/bin/shutdown -p +0
 ''
