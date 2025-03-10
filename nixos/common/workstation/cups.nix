9aef3f128b0de26338a9d856ed0757bf4e080b5f
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-03-10 13:04:33 -0400

    Changed the IP of the printer

--- nixos/common/workstation/cups.nix
+++ nixos/common/workstation/cups.nix
@@ -14,7 +14,7 @@
       {
         name = "HP_WiFi";
         location = "Home";
-        deviceUri = "http://192.168.0.5:631/hp/device";
+        deviceUri = "http://192.168.42.4:631/hp/device";
         model = "HP/hp-laserjet_tank_mfp_260x.ppd.gz";
         ppdOptions = {
           PageSize = "Letter";
