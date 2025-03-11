e387b1f125af462067da32c012f21c97b990290d
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-03-11 11:09:02 -0400

    Changed https to http

--- nixos/common/server/nginx.nix
+++ nixos/common/server/nginx.nix
@@ -64,7 +64,7 @@
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "https://192.168.0.7:8080";
+          proxyPass = "http://192.168.0.7:8080";
           proxyWebsockets = true;
         };
       };
