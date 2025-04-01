4bfae204e71c3d75a17bdfd077a2f362f6db2eca
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-04-01 14:28:12 -0400

    Updated the gnome configuration

--- nixos/common/workstation/gnome.nix
+++ nixos/common/workstation/gnome.nix
@@ -1,10 +1,7 @@
-{ pkgs, ... }:
+{ lib, ... }:
 {
-  programs.niri.enable = true;
-
-  services.upower.enable = true;
-
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gnome
-  ];
+  services.displayManager = {
+    autoLogin.enable = lib.mkForce false;
+  };
+  services.xserver.desktopManager.gnome.enable = true;
 }
