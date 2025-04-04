6d661d29992fb64176ce8f2042d1cf98f67a7461
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-04-04 12:38:39 -0400

    Made kitty to start up in full screen in gnome

--- home-manager/common/apps/desktop.nix
+++ home-manager/common/apps/desktop.nix
@@ -3,5 +3,12 @@ let
 in
 {
   xdg.desktopEntries = {
+    kitty = {
+      name = "Kitty";
+      genericName = "Terminal Emulator";
+      exec = "${pkgs.kitty}/bin/kitty --start-as=fullscreen %U";
+      terminal = false;
+      categories = [ "Application" "Terminal" ];
+    }
   };
 }
