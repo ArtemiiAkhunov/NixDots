4241babe9dd3cbf6e9efa3cc2312556efb7f0f06
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-04-04 13:06:10 -0400

    Changed kitty config to only apply to gnome

--- home-manager/common/apps/desktop.nix
+++ home-manager/common/apps/desktop.nix
@@ -3,12 +3,5 @@ let
 in
 {
   xdg.desktopEntries = {
-    kitty = {
-      name = "Kitty";
-      genericName = "Terminal Emulator";
-      exec = "${pkgs.kitty}/bin/kitty --start-as maximized %U";
-      terminal = false;
-      categories = [ "Utility" "TerminalEmulator" ];
-    };
   };
 }
---
+++ home-manager/common/de/desktop.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+let
+in
+{
+  xdg.desktopEntries = {
+    kitty = {
+      name = "Kitty";
+      genericName = "Terminal Emulator";
+      exec = "${pkgs.kitty}/bin/kitty --start-as maximized %U";
+      terminal = false;
+      categories = [ "Utility" "TerminalEmulator" ];
+    };
+  };
+}
