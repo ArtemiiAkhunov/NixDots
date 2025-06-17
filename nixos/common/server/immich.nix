af7ead8e76dc9406f6aab53940a7327cd88087ab
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-06-17 19:37:05 -0400

    fixed a typo

--- nixos/common/server/immich.nix
+++ nixos/common/server/immich.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  fileSystems."/data/immich/movies" = {
+  fileSystems."/data/immich/photos" = {
     device = "192.168.42.6:/mnt/pool/root/immich";
     fsType = "nfs";
     options = [
