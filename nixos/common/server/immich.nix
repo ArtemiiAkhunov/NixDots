39b70f2d70dcb3a546c61a5a5863750b3578565a
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-06-17 19:32:18 -0400

    Just left the mount for immich; will move it to docker

--- nixos/common/server/immich.nix
+++ nixos/common/server/immich.nix
@@ -1,22 +1,8 @@
-{ ... }:
-let
-  immichRoot = "/data/immich";
-in
-{
-  services.immich = {
-    enable = true;
-    openFirewall = true;
-    port = 2283;
-    mediaLocation = "${immichRoot}/photos";
-    settings.server.externalDomain = "https://immich.lordofthelags.net";
-    machine-learning.enable = false;
-  };
-
-  boot.supportedFilesystems = [ "nfs" ];
-  
-  fileSystems."${immichRoot}/photos" = {
+{}: {
+  fileSystems."/data/immich/movies" = {
     device = "192.168.42.6:/mnt/pool/root/immich";
     fsType = "nfs";
     options = [ "x-systemd.automount" "noauto" ];
   };
+
 }
