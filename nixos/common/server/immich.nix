5c5841e9c3b83e62e76c54f7bde11628ea79bf69
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-06-17 20:03:19 -0400

    something

--- nixos/common/server/immich.nix
+++ nixos/common/server/immich.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  fileSystems."/data/immich/photos" = {
+  fileSystems."/data/immich/movies" = {
     device = "192.168.42.6:/mnt/pool/root/immich";
     fsType = "nfs";
     options = [
