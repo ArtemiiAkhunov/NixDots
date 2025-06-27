c7da080265aade79a1dfa6f674c243c339271e66
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-06-26 21:03:30 -0400

    Fixed the issue with niri module

--- home-manager/common/wm/niri.nix
+++ home-manager/common/wm/niri.nix
@@ -1,18 +1,15 @@
 { pkgs, ... }:
 {
-  wayland.windowManager.niri = {
-    enable = true;
-    configFile = pkgs.replaceVars ./niri.kdl {
-      wireplumber = "${pkgs.wireplumber}";
-      copyq = "${pkgs.copyq}";
-      hyprlock = "${pkgs.hyprlock}";
-      wofi = "${pkgs.wofi}";
-      brightnessctl = "${pkgs.brightnessctl}";
-      kitty = "${pkgs.kitty}";
-      waybar = "${pkgs.waybar}";
-      satelite = "${pkgs.xwayland-satellite}";
-      swww = "${pkgs.swww}";
-      networkmanagerapplet = "${pkgs.networkmanagerapplet}";
-    };
+  xdg.configFile."niri/config.kdl" = pkgs.replaceVars ./niri.kdl {
+    wireplumber = "${pkgs.wireplumber}";
+    copyq = "${pkgs.copyq}";
+    hyprlock = "${pkgs.hyprlock}";
+    wofi = "${pkgs.wofi}";
+    brightnessctl = "${pkgs.brightnessctl}";
+    kitty = "${pkgs.kitty}";
+    waybar = "${pkgs.waybar}";
+    satelite = "${pkgs.xwayland-satellite}";
+    swww = "${pkgs.swww}";
+    networkmanagerapplet = "${pkgs.networkmanagerapplet}";
   };
 }
