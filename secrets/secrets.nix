6b294907d91fc2b159804adb46becd6a70e2c59a
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-07-16 12:00:03 -0400

    Re-encrypted files

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   voidwalker_laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZnYBYqrL2oHZwASlFxmGZBGyS06QrO79jdnJbVwaDn voidwalker@kamigawa";
-  voidwalker_server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCInIkVTDgRGaBsplan/7yRPLd06yoQ1ARYKHzRCgho voidwalker@theros";
+  voidwalker_server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGu2Ti/hBRJOKfu2WnWWBbHbt70vz+QbeNHobcCySWCM voidwalker@theros";
   users = [
     voidwalker_server
     voidwalker_laptop
