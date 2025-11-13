deaf8cc24e6fa20f8e6bceeee63949a6b7fffe64
ArtemiiAkhunov <artemii13052003@gmail.com>
2025-11-13 09:57:05 -0500

    Updated nginx

--- nixos/common/server/nginx.nix
+++ nixos/common/server/nginx.nix
@@ -114,6 +114,14 @@
           proxyWebsockets = true;
         };
       };
+      "jellyseer.lordofthelags.net" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:5055";
+          proxyWebsockets = true;
+        };
+      };
     };
   };
 }
