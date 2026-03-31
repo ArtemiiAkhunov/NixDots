87e53b4fdb592f70b2ef0f6b9014666df6799a06
ArtemiiAkhunov <artemii13052003@gmail.com>
2026-03-31 11:18:37 -0500

    Fixed the battery monitor systemd service

--- nixos/common/workstation/battmon.nix
+++ nixos/common/workstation/battmon.nix
@@ -1,30 +1,33 @@
 {pkgs, ...}: 
 let
-  battmon = ''
+  battmon = pkgs.writeShellScriptBin "battmon" ''
     HIGH_THRESHOLD=95
     LOW_THRESHOLD=10
     LOGFILE="/tmp/battmon.log"
 
     # Get the battery level
-    LEVEL=$(acpi -b | awk -F', ' '{print $2}' | tr -d '%,')
+    LEVEL=$(${pkgs.acpi}/bin/acpi -b | awk -F', ' '{print $2}' | tr -d '%,')
 
     # Ensure LEVEL is a valid number
     if [[ "$LEVEL" =~ ^[0-9]+$ ]]; then
         # Check for high battery level
         if [ "$LEVEL" -ge "$HIGH_THRESHOLD" ]; then
             echo "$(date) - Battery at $LEVEL%. Sending high battery notification..." >> "$LOGFILE"
-            DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 notify-send -t 0 "Battery Full" "Your battery is now fully charged." >> "$LOGFILE" 2>&1
+            DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 ${pkgs.libnotify}/bin/notify-send -t 0 "Battery Full" "Your battery is now fully charged." >> "$LOGFILE" 2>&1
         fi
 
         # Check for low battery level
         if [ "$LEVEL" -le "$LOW_THRESHOLD" ]; then
             echo "$(date) - Battery at $LEVEL%. Sending low battery warning..." >> "$LOGFILE"
-            DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 notify-send -t 0 "Battery Low" "Your battery is critically low. Please plug in your charger!" >> "$LOGFILE>
+            DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 ${pkgs.libnotify}/bin/notify-send -t 0 "Battery Low" "Your battery is critically low. Please plug in your charger!" >> "$LOGFILE"
         fi
     fi
   '';
 in
 {
+  environment.systemPackages = [
+    battmon
+  ];
   systemd.timers."battery-check" = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
@@ -35,10 +38,13 @@ in
   };
 
   systemd.services."battery-check" = {
-    script = battmon;
+    script = ''
+      ${battmon}/bin/battmon
+    '';
     serviceConfig = {
       Type = "oneshot";
       User = "root";
+      RunAfterExit = true;
     };
   };
 }
