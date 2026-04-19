0417479885c88c8d70ec02717a3355d6d8cc08ba
ArtemiiAkhunov <artemii13052003@gmail.com>
2026-04-19 16:54:10 -0500

    Added printer support to workstation

--- nixos/common/workstation/cups.nix
+++ nixos/common/workstation/cups.nix
@@ -1,11 +1,37 @@
 { pkgs, ... }:
+let
+  versalinkB615 = builtins.fetchurl {
+    url = "https://answers.uillinois.edu/images/group356/shared/xrxb615.ppd";
+    sha256 = "02j8xngin7ijqd7xa8lzf2lkls75j4fdym6bb2811kwz1zwrpiqx";
+  };
+  phaser5550 = builtins.fetchurl {
+    url = "https://answers.uillinois.edu/images/group356/shared/xr5550dt.ppd";
+    sha256 = "1001hnjqpz5djdiqh3q9dgswzjx517pmx1na7if4slzbyfya79b4";
+  };
+in 
 {
+  services.avahi = {
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+
   services.printing = {
     enable = true;
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
     drivers = with pkgs; [
       gutenprint
       gutenprintBin
       hplip
+      (writeTextDir "share/cups/model/xrxb615.ppd" (builtins.readFile versalinkB615))
+      (writeTextDir "share/cups/model/xr5550dt.ppd" (builtins.readFile phaser5550))
+
     ];
   };
 
--- nixos/common/workstation/packages.nix
+++ nixos/common/workstation/packages.nix
@@ -76,6 +76,7 @@ in
 
       nss
       vscode
+      claude-code
 
       # School Apps
 
