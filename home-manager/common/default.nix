d8abc27debdc64e583a7a24e01f0258336e330c1
ArtemiiAkhunov <artemii13052003@gmail.com>
2026-05-13 12:23:40 -0500

    Fixed home-manager issue

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -1,7 +1,10 @@
-{
+{lib, ...}: {
   imports = [
     ./shell
     ./nixvim
     ./terminal/ssh.nix
+  ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "cmp-emoji"
   ];
 }
