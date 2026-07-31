c074e6224984d5b880de7ea36c2ef9737ed4c013
Artemii Akhunov <33598448+ArtemiiAkhunov@users.noreply.github.com>
2026-07-30 20:03:05 -0700

    Eval Issue Hotfix (#7)
    Updated the PPDs into one derivation.

--- nixos/common/workstation/cups.nix
+++ nixos/common/workstation/cups.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }:
 let
-  versalinkB615 = builtins.fetchurl {
+  versalinkB615 = pkgs.fetchurl {
     url = "https://answers.uillinois.edu/images/group356/shared/xrxb615.ppd";
     sha256 = "02j8xngin7ijqd7xa8lzf2lkls75j4fdym6bb2811kwz1zwrpiqx";
   };
-  phaser5550 = builtins.fetchurl {
+  phaser5550 = pkgs.fetchurl {
     url = "https://answers.uillinois.edu/images/group356/shared/xr5550dt.ppd";
     sha256 = "1001hnjqpz5djdiqh3q9dgswzjx517pmx1na7if4slzbyfya79b4";
   };
+  xeroxPpds = pkgs.runCommand "xerox-ppds" { } ''
+    install -Dm444 ${versalinkB615} $out/share/cups/model/xrxb615.ppd
+    install -Dm444 ${phaser5550} $out/share/cups/model/xr5550dt.ppd
+  '';
 in
 {
   services.avahi = {
@@ -29,9 +33,7 @@ in
       gutenprint
       gutenprintBin
       hplip
-      (writeTextDir "share/cups/model/xrxb615.ppd" (builtins.readFile versalinkB615))
-      (writeTextDir "share/cups/model/xr5550dt.ppd" (builtins.readFile phaser5550))
-
+      xeroxPpds
     ];
   };
 
