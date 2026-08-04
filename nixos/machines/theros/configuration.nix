e67cd835be10e0f9701697e4d21881800c544eb9
Artemii Akhunov <33598448+ArtemiiAkhunov@users.noreply.github.com>
2026-08-03 17:27:54 -0700

    Auto Updates (#8)
    Nightly updates on my server and periodic updates for the laggy-server
    flake input

--- nixos/machines/theros/configuration.nix
+++ nixos/machines/theros/configuration.nix
@@ -3,5 +3,12 @@
 
   networking.hostName = "theros";
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = "github:ArtemiiAkhunov/NixDots";
+    dates = "04:30";
+    randomizedDelaySec = "30min";
+  };
+
   system.stateVersion = "24.11"; # DO NOT TOUCH
 }
