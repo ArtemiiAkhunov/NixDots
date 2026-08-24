d0046f5da2da6bbaec93e6d4c203c442659264e4
Artemii Akhunov <33598448+ArtemiiAkhunov@users.noreply.github.com>
2026-08-24 04:03:30 +0000

    Updated ssh keys (#16)
    Added keys for kaldheim

--- nixos/common/server/ssh.nix
+++ nixos/common/server/ssh.nix
@@ -8,6 +8,7 @@
   };
 
   users.users.voidwalker.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA220xmCC5RCUXRjTuEV87z4wPDsbn8IXtbMpaUwiWr9 voidwalker@kaldheim"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILZnYBYqrL2oHZwASlFxmGZBGyS06QrO79jdnJbVwaDn voidwalker@kamigawa"
   ];
 }
